9c7923548f2c6f5dfe38b747219fe4d972d45660
Captain-Coomer <105549755+Captain-Coomer@users.noreply.github.com>
2022-05-14 19:53:43 +0300

    Add files via upload

---
+++ Homework#SefaBILSEL.py
@@ -0,0 +1,18 @@
+# Program: Compare 2 numbers
+
+# Entering the 1st number
+x1 = int(input("Please Enter the 1st number: "))
+
+# Entering the 2nd number
+x2 = int(input("Please Enter the 2nd number: "))
+
+# Comparing x1 and x2
+if x1 > x2:
+    print("1st number is greater than 2nd number")
+else:
+    print("2nd number is greater than 1st number")
+
+# 2nd method
+
+# Returns True if x1 is greater than x2 else returns False
+print(x1 > x2)
